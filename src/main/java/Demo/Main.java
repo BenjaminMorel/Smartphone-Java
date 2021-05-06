c0c7a95ca375b933a9be2fd63da251d67ff204df
choffat <theo.choffat@student.hevs.ch>
2021-05-06 10:24:02 +0200

    Date, Heure, HomeScreen wallpaper and time, date Class Time

--- src/main/java/Demo/Main.java
+++ src/main/java/Demo/Main.java
@@ -1,5 +1,6 @@
 package Demo;
 
+import General.CloseButton;
 import Screen.HomeScreen;
 
 import java.awt.*;
@@ -7,7 +8,11 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+        //CloseButton closeButton = new CloseButton();
         Smartphone smartphone = new Smartphone() ;
+
+
+        //closeButton.setVisible(true);
         smartphone.setVisible(true);
 
     }
---
+++ src/main/java/General/CloseButton.java
@@ -0,0 +1,14 @@
+package General;
+
+import Demo.Smartphone;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CloseButton extends JButton {
+    JButton button;
+
+    public CloseButton() {
+        button = new JButton();
+    }
+}
